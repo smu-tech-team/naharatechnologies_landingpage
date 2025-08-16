31090f5c69569783d084a68f88edf68dfcc4a034
Clinton Ngotta <clintonngotta@gmail.com>
2025-08-16 18:38:48 +0300

    fixed navbar closing issue on mobile
    added Next Link to improve speed and avoid page rerender

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,49 @@
 import Link from "next/link";
 import React from "react";
 
-const NavBar = ({ styling }: { styling: string }) => {
-  return (
-    <nav className={styling}>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <a href="/upcomingproducts">Products to be released</a>
-      </li>
-      <li>
-        <a href="/career">Career</a>
-      </li>
-      <li>
-        <a href="/customer">Customers</a>
-      </li>
-      <li>
-        <a href="/help">Help</a>
-      </li>
-      <Link href="/contact">
-        <button className="bg-black text-white font-medium px-4 py-2 inline-flex items-center justify-center tracking-tight">
-          Contact Us
-        </button>
-      </Link>
-    </nav>
-  );
+const NavBar = ({
+	styling,
+	setIsMobileNavOpen,
+}: {
+	styling: string;
+	setIsMobileNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+	return (
+		<nav className={styling}>
+			<>
+				<Link onClick={() => setIsMobileNavOpen(false)} href='/about'>
+					About
+				</Link>
+			</>
+			<li>
+				<Link
+					onClick={() => setIsMobileNavOpen(false)}
+					href='/upcomingproducts'>
+					Products to be released
+				</Link>
+			</li>
+			<li>
+				<Link onClick={() => setIsMobileNavOpen(false)} href='/career'>
+					Career
+				</Link>
+			</li>
+			<li>
+				<Link onClick={() => setIsMobileNavOpen(false)} href='/customer'>
+					Customers
+				</Link>
+			</li>
+			<li>
+				<Link onClick={() => setIsMobileNavOpen(false)} href='/help'>
+					Help
+				</Link>
+			</li>
+			<Link onClick={() => setIsMobileNavOpen(false)} href='/contact'>
+				<button className='bg-black text-white font-medium px-4 py-2 inline-flex items-center justify-center tracking-tight'>
+					Contact Us
+				</button>
+			</Link>
+		</nav>
+	);
 };
 
 export default NavBar;
