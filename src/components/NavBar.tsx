8a4ba3fc2465ce10107c24b9326cbfd651d2ef62
Ebelechukwu Doris Okolo <dorisebele47@gmail.com>
2025-08-30 11:38:21 +0100

    adjusted some design

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,48 +2,54 @@ import Link from "next/link";
 import React from "react";
 
 const NavBar = ({
-	styling,
-	setIsMobileNavOpen,
+  styling,
+  setIsMobileNavOpen,
 }: {
-	styling: string;
-	setIsMobileNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  styling: string;
+  setIsMobileNavOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
-	return (
-		<nav className={styling}>
-			<>
-				<Link onClick={() => setIsMobileNavOpen(false)} href='/about'>
-					About
-				</Link>
-			</>
-			<li>
-				<Link
-					onClick={() => setIsMobileNavOpen(false)}
-					href='/upcomingproducts'>
-					Products to be released
-				</Link>
-			</li>
-			<li>
-				<Link onClick={() => setIsMobileNavOpen(false)} href='/career'>
-					Career
-				</Link>
-			</li>
-			<li>
-				<Link onClick={() => setIsMobileNavOpen(false)} href='/customer'>
-					Customers
-				</Link>
-			</li>
-			<li>
-				<Link onClick={() => setIsMobileNavOpen(false)} href='/help'>
-					Help
-				</Link>
-			</li>
-			<Link onClick={() => setIsMobileNavOpen(false)} href='/contact'>
-				<button className='bg-black text-white font-medium px-4 py-2 inline-flex items-center justify-center tracking-tight'>
-					Contact Us
-				</button>
-			</Link>
-		</nav>
-	);
+  const linkClasses =
+    "relative font-semibold tracking-wide text-black after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px] after:w-0 after:bg-black after:transition-all after:duration-300 hover:after:w-full";
+
+  return (
+    <nav className={styling}>
+      <>
+        <Link
+          onClick={() => setIsMobileNavOpen(false)}
+          href="/about"
+          className={linkClasses}
+        >
+          About
+        </Link>
+      </>
+      <li>
+        <Link
+          onClick={() => setIsMobileNavOpen(false)}
+          href="/career"
+          className={linkClasses}
+        >
+          Career
+        </Link>
+      </li>
+      <li>
+        <Link
+          onClick={() => setIsMobileNavOpen(false)}
+          href="/help"
+          className={linkClasses}
+        >
+          Help
+        </Link>
+      </li>
+      <Link
+        onClick={() => setIsMobileNavOpen(false)}
+        href="/contact"
+      >
+        <button className="bg-black text-white font-medium px-4 py-2 inline-flex items-center justify-center tracking-tight">
+          Contact Us
+        </button>
+      </Link>
+    </nav>
+  );
 };
 
 export default NavBar;
--- src/sections/MissionVision.tsx
+++ src/sections/MissionVision.tsx
@@ -3,20 +3,21 @@
 import ImageWrap from "@/components/ImageWrap";
 import LottieAnim from "@/components/LottieAnim";
 import TitleDescription from "@/components/TitleDescription";
-import { images, lottieAnims, missionText } from "@/constants";
+import { images, lottieAnims, missionText, visionText } from "@/constants";
 import React from "react";
 
 const MissionVision = () => {
   return (
 		<div className='px-6 py-10 bg-black lg:px-[105px]'>
 			<div className='container'>
-				<TitleDescription
-					styling='max-w-[694px] text-white'
-					title='Our Mission'
-					desc={missionText}
+			<TitleDescription
+					styling='max-w-[694px] ml-auto text-white'
+					title='Our Vision'
+					desc={visionText}
 					titleStyle='text-lg font-bold'
 					descStyle='mt-1'
 				/>
+				
 
 				{/* <LottieAnim lottieFile={lottieAnims.globe} styling="size-full my-5" /> */}
 				<LottieAnim
@@ -25,9 +26,9 @@ const MissionVision = () => {
 					animClass='h-64'
 				/>
 
-				<TitleDescription
-					styling='max-w-[694px] ml-auto text-white'
-					title='Our Vision'
+                <TitleDescription
+					styling='max-w-[694px] text-white'
+					title='Our Mission'
 					desc={missionText}
 					titleStyle='text-lg font-bold'
 					descStyle='mt-1'
